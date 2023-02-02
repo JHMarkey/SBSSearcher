f0ab4e3ca6fa4080caddd6490961232eafee2518
JHMarkey <95226970+JHMarkey@users.noreply.github.com>
2023-02-02 18:16:02 +0000

    Add files via upload
    Initial Commit

---
+++ View/_inc/header.php
@@ -0,0 +1,11 @@
+<body style = "margin-top: 0">
+  <div class = "header">
+    <div class="inner_header">
+      <div class="logo_container">
+        <h1>SBS</h1>
+      </div>
+      <ul class = "navigation">
+        <a href = "../View/index.php"><li>HomePage</li></a>        
+			</ul>
+    </div>
+  </div>
