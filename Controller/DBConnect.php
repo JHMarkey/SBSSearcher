cada687f4093bbd7426a0a39668d63a2c8472cc6
JHMarkey <c1009859@my.shu.ac.uk>
2023-02-14 11:48:10 +0000

    style + index

--- Controller/DBConnect.php
+++ Controller/DBConnect.php
@@ -10,7 +10,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-/*$connectionInfo = array("UID" => "phpdbLogin", "pwd" => "php-Password123", "Database" => "sbsdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "phpdbLogin", "pwd" => "php-Password123", "Database" => "sbsdb", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:sbss.database.windows.net,1433";
-$conn = sqlsrv_connect($serverName, $connectionInfo);*/
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
