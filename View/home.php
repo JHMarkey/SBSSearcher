a99aefa8c2b658d10333a5292e4f377050726779
LKingy7 <80429251+LKingy7@users.noreply.github.com>
2023-02-15 10:12:51 +0000

    Watson Creation Clean Up

--- View/home.php
+++ View/home.php
@@ -23,6 +23,9 @@ require("../Controller/DBConnect.php");
     </div>
 </div>
 </body>
+
+<script type = "text/javascript" src="../View/scripts/WatsonAssistantCreation.js"></script>
+
 <script>
     $(document).ready(function() {
         var chatBox = $('#chat-box');
@@ -32,20 +35,11 @@ require("../Controller/DBConnect.php");
             chatBox.append('<p>You: ' + message + '</p>');
             $('.form-control').val('');
 
-            window.watsonAssistantChatOptions = {
-            integrationID: "f1ffc42f-5126-4917-90b1-875d66d8af1a", // The ID of this integration.
-            region: "eu-gb", // The region your integration is hosted in.
-            serviceInstanceID: "e6aa3b1c-7516-4dbb-bff0-4359e072de17", // The ID of your service instance.
-            onLoad: function(instance) { instance.render(); }
-        };
-        setTimeout(function(){
-            const t=document.createElement('script');
-            t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
-            document.head.appendChild(t);
-        });
+            
         });
     });
 </script>
+
 <?php
 require("../View/_inc/Footer.php");
 ?>
--- View/scripts/WatsonAssistantCreation.js
+++ View/scripts/WatsonAssistantCreation.js
@@ -1,10 +1,11 @@
-const AssistantV2 = require('ibm-watson/assistant/v2');
-const { IamAuthenticator } = require('ibm-watson/auth');
-
-const assistant = new AssistantV2({
-  version: '{version}',
-  authenticator: new IamAuthenticator({
-    apikey: '{apikey}',
-  }),
-  serviceUrl: 'https://api.eu-gb.assistant.watson.cloud.ibm.com',
+window.watsonAssistantChatOptions = {
+  integrationID: "f1ffc42f-5126-4917-90b1-875d66d8af1a", // The ID of this integration.
+  region: "eu-gb", // The region your integration is hosted in.
+  serviceInstanceID: "e6aa3b1c-7516-4dbb-bff0-4359e072de17", // The ID of your service instance.
+  onLoad: function(instance) { instance.render(); }
+};
+setTimeout(function(){
+  const t=document.createElement('script');
+  t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+  document.head.appendChild(t);
 });
