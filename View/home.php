eb7ed1d6e404dbe9aa5abb66416228496a410964
LKingy7 <80429251+LKingy7@users.noreply.github.com>
2023-02-15 09:49:07 +0000

    Basic Home Page Functionality

--- View/home.php
+++ View/home.php
@@ -4,61 +4,48 @@ require("../View/_inc/header.php");
 require("../Controller/DBConnect.php");
 ?>
 
-
-  <head>
-    <title>My Page</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
-  </head>
-  <body>
-    <div class="container-fluid">
-      <div class="row">
-        <div class="col-3 bg-light">
-          <nav class="navbar navbar-expand-md navbar-light">
-            <a class="navbar-brand" href="#">My Site</a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav">
-              <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-              <ul class="navbar-nav">
-                <li class="nav-item active">
-                  <a class="nav-link" href="#">Home</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">About</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="#">Contact</a>
-                </li>
-              </ul>
-            </div>
-          </nav>
-        </div>
-        <div class="col-9">
-          <div class="row">
-            <div class="col">
-              <h1>Welcome to My Site</h1>
-              <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla auctor aliquet odio, quis mollis purus efficitur vitae.</p>
-            </div>
-          </div>
-          <div class="row">
-            <div class="col">
-              <form>
-                <div class="form-group">
-                  <label for="backendTextarea">Talk to the backend:</label>
-                  <textarea class="form-control" id="backendTextarea" rows="3"></textarea>
+<head>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+</head>
+<body>
+    <div class="container">
+    <div class="row">
+        <div class="col-md-12">
+            <h1>Wade </h1>
+            <div id="chat-box"></div>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" placeholder="Type your message...">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="button" id="send-btn">Send</button>
                 </div>
-              </form>
             </div>
-          </div>
         </div>
-      </div>
     </div>
-  </body>
+</div>
+</body>
+<script>
+    $(document).ready(function() {
+        var chatBox = $('#chat-box');
 
+        $('#send-btn').on('click', function() {
+            var message = $('.form-control').val();
+            chatBox.append('<p>You: ' + message + '</p>');
+            $('.form-control').val('');
+
+            window.watsonAssistantChatOptions = {
+            integrationID: "f1ffc42f-5126-4917-90b1-875d66d8af1a", // The ID of this integration.
+            region: "eu-gb", // The region your integration is hosted in.
+            serviceInstanceID: "e6aa3b1c-7516-4dbb-bff0-4359e072de17", // The ID of your service instance.
+            onLoad: function(instance) { instance.render(); }
+        };
+        setTimeout(function(){
+            const t=document.createElement('script');
+            t.src="https://web-chat.global.assistant.watson.appdomain.cloud/versions/" + (window.watsonAssistantChatOptions.clientVersion || 'latest') + "/WatsonAssistantChatEntry.js";
+            document.head.appendChild(t);
+        });
+        });
+    });
+</script>
 <?php
 require("../View/_inc/Footer.php");
 ?>
