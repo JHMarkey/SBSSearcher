82d52ad6e87c0345258888216562e75108ab083f
JHMarkey <c1009859@my.shu.ac.uk>
2023-02-16 15:07:35 +0000

    comb

--- View/_inc/loggedHeader.php
+++ View/_inc/loggedHeader.php
@@ -12,9 +12,8 @@ function drawHeader(){?>
             echo($_SESSION["FN"]. " ". $_SESSION["SN"])
             ?></li>
         </a>         
-        <a href = "../Controller/Logout.php"><li>Log Out</li></a>
-        <a href = "leaderboard.php"><li>Leader Board</li></a> 
-		</ul>
+        <a href = "index.php"><li>Log Out</li></a>
+      </ul>
     </div>
   </div>
 
